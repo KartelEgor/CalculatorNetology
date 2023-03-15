d9454367fddfe60c804f0119a8c0387116519637
Kartel Egor <dimurg34@gmail.com>
2023-03-15 12:42:57 +0300

    remade

--- Calculator.java
+++ Calculator.java
@@ -7,10 +7,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        if (x == 0 || y == 0) return 0;
-        else return x / y;
-    }; // проблему исключения решил блоком кода с return.
+    BinaryOperator<Integer> devide = (x, y) -> y > 0 ? x/y : y; // тернарный оператор
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
